af18b5c98093a6fa83865f59c05657b9b737d1ad
임권 <limkwon18@gmail.com>
2024-10-28 17:31:19 +0900

    init : backend

---
+++ backend/src/test/java/com/clofit/ClofitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clofit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClofitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
