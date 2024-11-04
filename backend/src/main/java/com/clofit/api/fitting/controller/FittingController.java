c3901126c7a2628c362ba14178fc1e7b4121f13b
강지우 <jiu6525@daum.net>
2024-11-04 17:44:57 +0900

    feat : s3 파일 업로드 삭제

--- backend/src/main/java/com/clofit/api/fitting/controller/FittingController.java
+++ backend/src/main/java/com/clofit/api/fitting/controller/FittingController.java
@@ -1,31 +1,28 @@
 package com.clofit.api.fitting.controller;
 
+import com.clofit.api.fitting.service.AwsS3Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.List;
-
-@RequiredArgsConstructor
 @RestController
+@RequestMapping("/fitting")
+@RequiredArgsConstructor
 public class FittingController {
-//    private final AwsS3Service awsS3Service;
-//
-//    @PostMapping
-//    public ResponseEntity<List<String>> uploadFile(List<MultipartFile> multipartFiles){
-//        return ResponseEntity.ok(awsS3Service.uploadFile(multipartFiles));
-//    }
-//
-//    @DeleteMapping
-//    public ResponseEntity<String> deleteFile(@RequestParam String fileName){
-//        awsS3Service.deleteFile(fileName);
-//        return ResponseEntity.ok(fileName);
-//    }
+    private final AwsS3Service awsS3Service;
+
+    @PostMapping
+    public ResponseEntity<String> uploadFile(MultipartFile multipartFile){
+        return ResponseEntity.ok(awsS3Service.uploadFile(multipartFile));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> deleteFile(@RequestParam String fileName){
+        awsS3Service.deleteFile(fileName);
+        return ResponseEntity.ok(fileName);
+    }
 
 }
+
+
