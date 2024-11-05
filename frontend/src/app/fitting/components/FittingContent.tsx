6b6dd2c102578f139f54b2a322d123d4f840777b
kkkhhh4930 <kkkhhh4930@naver.com>
2024-11-05 14:54:25 +0900

    feat: 피팅 추가 컴포넌트

--- frontend/src/app/fitting/components/FittingContent.tsx
+++ frontend/src/app/fitting/components/FittingContent.tsx
@@ -1,45 +1,14 @@
-import { GiClothes } from 'react-icons/gi';
+import React from 'react';
+import FittingStart from './FittingStart';
 
 interface FittingContentProps {
-  hasData: boolean;
   onStartFitting: () => void;
-  fittingImage: string;
-  loading: boolean;
 }
 
-function FittingContent({
-  hasData,
-  onStartFitting,
-  fittingImage,
-  loading,
-}: FittingContentProps) {
-  if (loading) {
-    return <div>로딩 중...</div>; // 로딩 상태 표시
-  }
-
+function FittingContent({ onStartFitting }: FittingContentProps) {
   return (
-    <div className='content'>
-      {hasData ? (
-        <div className='fitting-result'>
-          <img
-            src={fittingImage}
-            alt='가상 피팅 결과'
-            className='fitting-image'
-          />
-        </div>
-      ) : (
-        <div className='no-data-container'>
-          <GiClothes size={48} color='gray' className='no-data-icon' />
-          <p className='no-data-message'>
-            추가한 옷장 아이템으로
-            <br />
-            가상피팅을 시작해 보세요.
-          </p>
-          <button onClick={onStartFitting} className='start-button'>
-            피팅 하러 가기
-          </button>
-        </div>
-      )}
+    <div className='flex justify-center items-start min-h-screen pt-20'>
+      <FittingStart onStartFitting={onStartFitting} />
     </div>
   );
 }
---
+++ frontend/src/app/fitting/components/FittingStart.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import ButtonRounded from '@/components/ButtonRounded';
+
+interface FittingStartProps {
+  onStartFitting: () => void;
+}
+
+function FittingStart({ onStartFitting }: FittingStartProps) {
+  return (
+    <div className='flex flex-col items-center text-center p-8 bg-white rounded-2xl'>
+      {/* 아이콘 */}
+      <img
+        src='/images/fitting-inactive.svg'
+        alt='피팅 아이템 없음'
+        className='w-20 h-20 mb-5'
+      />
+
+      {/* 안내 텍스트 */}
+      <p className='text-lg mb-5 leading-relaxed'>
+        추가한 옷장 아이템으로
+        <br />
+        가상피팅을 시작해 보세요.
+      </p>
+
+      {/* 둥근 버튼 */}
+      <ButtonRounded text='피팅 하러 가기' onClick={onStartFitting} />
+    </div>
+  );
+}
+
+export default FittingStart;
