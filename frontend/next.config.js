c745b804992446cace20b79457e35dd28dc3d5fc
kkkhhh4930 <kkkhhh4930@naver.com>
2024-11-05 09:29:00 +0900

    feat: 마이 페이지 레이아웃

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,7 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development', // 개발 모드에서 PWA 비활성화
 });
 
 module.exports = withPWA({
--- frontend/src/app/closet/page.tsx
+++ frontend/src/app/closet/page.tsx
@@ -1,3 +1,37 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import LeftTitleTopbar from '@/components/LeftTitleTopbar';
+import EmptyCloset from './components/EmptyCloset';
+import FilledCloset from './components/FilledCloset';
+
 export default function Closet() {
-  return <div>옷장 페이지</div>;
+  const [hasItems, setHasItems] = useState(true);
+
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  // 아이템 조회 함수.
+  async function fetchItems() {
+    try {
+      // const response = await fetch('/api/items'); // 예시 API 엔드포인트
+      // const data = await response.json();
+      // setHasItems(data.length > 0); // 데이터가 있으면 true, 없으면 false로 설정
+
+      // 임시로 아이템이 있다고 설정
+      setHasItems(true);
+    } catch (error) {
+      console.error('아이템 로딩 중 에러 발생:', error);
+    }
+  }
+
+  return (
+    <div>
+      <LeftTitleTopbar title='옷장' />
+      <div className='content'>
+        {hasItems ? <FilledCloset /> : <EmptyCloset />}
+      </div>
+    </div>
+  );
 }
