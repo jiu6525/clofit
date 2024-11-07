2be84965a0db73fc1c9bb109c7889aa2eaa100e6
YuJin Son <156387293+yjjj1612@users.noreply.github.com>
2024-11-07 14:23:06 +0900

    feat: 옷장 페이지
    Former-commit-id: 03ec85c2ff804bc2f2430c461dddb6d5384aa26f

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -2,24 +2,28 @@
 
 import React from 'react';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 import Navbar from '@/components/Navbar';
 
 export default function MainPage() {
+  const router = useRouter();
   const placeholderImage = '/snap1.webp';
 
   return (
     <div className='w-full min-h-screen bg-white flex justify-center'>
       {/* 최상위 컨테이너 설정 */}
       <div className='w-full min-w-[360px] max-w-[600px] p-4 flex flex-col items-center'>
-        <header className='w-full py-4 px-2 text-left'>
+        <header className='w-full py-4 px-2 mt-2 text-left'>
           <Image src='/logo.svg' alt='Clofit Logo' width={80} height={30} />
         </header>
 
         {/* 등록 배너 */}
-        <div className='w-full bg-[#464646] text-white py-2 px-4 flex items-center justify-between rounded-md my-4'>
+        <div className='w-full bg-[#464646] py-2 px-4 flex items-center justify-between rounded-md my-4'>
           <div>
-            <p className='text-sm'>나만의 디지털 옷장을 만들고 싶다면</p>
-            <p className='text-base font-semibold mt-1'>
+            <p className='text-sm text-[#B7B7B7] ml-2 font-semibold'>
+              나만의 디지털 옷장을 만들고 싶다면
+            </p>
+            <p className='text-base text-white ml-2 font-semibold mt-1'>
               간편하게 내 아이템 등록하기
             </p>
           </div>
@@ -30,7 +34,7 @@ export default function MainPage() {
 
         {/* 추천 아이템 섹션 */}
         <section className='w-full'>
-          <h2 className='text-base font-medium mb-4'>
+          <h2 className='text-base font-medium text-[#373A3F] my-4'>
             강현후님을 위한 아이템 추천
           </h2>
           <div className='grid grid-cols-3 gap-0'>
@@ -47,7 +51,7 @@ export default function MainPage() {
             ))}
           </div>
 
-          <h2 className='text-base font-medium my-4'>
+          <h2 className='text-base font-medium text-[#373A3F] my-4'>
             강현후님이 좋아요 한 코디
           </h2>
 
@@ -60,21 +64,31 @@ export default function MainPage() {
                   fill
                   className='object-cover'
                 />
-                <p className='text-center text-sm mt-2'>무신사 스탠다드</p>
               </div>
             ))}
           </div>
         </section>
+
         {/* 가상 피팅 추천 배너 */}
-        <div className='w-full bg-[#E6F0FF] p-4 rounded mt-6 mb-16 flex items-center justify-between gap-4'>
+        <div
+          className='w-full bg-[#E6F0FF] p-4 rounded mt-6 mb-16 flex items-center justify-between gap-4 cursor-pointer'
+          onClick={() => router.push('/fitting')}
+        >
           <div className='flex flex-col items-center text-center'>
-            <p className='text-lg font-semibold text-[#373A3F]'>
+            <p className='text-lg font-bold text-[#222222]'>
               나와 어울리는 아이템은?
             </p>
-            <p className='text-blue-600 font-semibold'>가상피팅 하러가기</p>
+            <p className='text-lg text-[#3376F6] font-bold'>
+              가상피팅 하러가기
+            </p>
           </div>
           <div>
-            <Image src='/closet.svg' alt='Closet Icon' width={60} height={60} />
+            <Image
+              src='/gofitting.svg'
+              alt='Closet Icon'
+              width={105}
+              height={60}
+            />
           </div>
         </div>
 
