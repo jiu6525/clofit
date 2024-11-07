5f95c96a65a83f02d5cdb2b4d797377cc3ff95ad
임권 <limkwon18@gmail.com>
2024-11-07 15:20:27 +0900

    update

--- backend/src/main/java/com/clofit/config/SecurityConfig.java
+++ backend/src/main/java/com/clofit/config/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig {
         this.jwtUtil = jwtUtil;
         this.ACCESS_KEY = ACCESS_KEY;
     }
-    @Order(2)
+    @Order(1)
     @Bean
     public SecurityFilterChain filterChainJWT(HttpSecurity http) throws Exception {
         http
@@ -90,13 +90,13 @@ public class SecurityConfig {
         
         //ignore를 제외한 모든 경로에 대해 인증 요구
         //ORDER로 인해 GPU 설정이 우선된다.
-        http.authorizeHttpRequests(auth -> auth.requestMatchers("/api/oauth2/authorization/**", "oauth2/authorization/**").permitAll().anyRequest().authenticated());
-//        http.authorizeHttpRequests(auth -> auth.requestMatchers("/**").authenticated().anyRequest().permitAll());
+//        http.authorizeHttpRequests(auth -> auth.requestMatchers("/api/oauth2/authorization/**", "oauth2/authorization/**").permitAll().anyRequest().authenticated());
+        http.authorizeHttpRequests(auth -> auth.requestMatchers("/**").authenticated().anyRequest().permitAll());
 
         return http.build();
     }
 
-    @Order(1)
+    @Order(2)
     @Bean
     public SecurityFilterChain filterChainGPU(HttpSecurity http) throws Exception {
 
