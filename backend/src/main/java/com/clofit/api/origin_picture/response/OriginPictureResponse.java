86341e5cf1e3221896bda73886f95e9a317e9646
kinterlocked <dusehd0525@naver.com>
2024-11-11 13:47:34 +0900

    feat: "유저 원본 사진관련 api 구현"

---
+++ backend/src/main/java/com/clofit/api/origin_picture/response/OriginPictureResponse.java
@@ -0,0 +1,16 @@
+package com.clofit.api.origin_picture.response;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OriginPictureResponse {
+
+    private Long OriginPictureId;
+    private String imgUrl;
+
+}
