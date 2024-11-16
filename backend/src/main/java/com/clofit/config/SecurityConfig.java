0e16adb3e4492bba00028ad27a49899192869084
임권 <limkwon18@gmail.com>
2024-11-17 00:42:53 +0900

    update

--- backend/src/main/java/com/clofit/config/SecurityConfig.java
+++ backend/src/main/java/com/clofit/config/SecurityConfig.java
@@ -127,7 +127,8 @@ public class SecurityConfig {
                 "/swagger-ui/**",
                 "/swagger-resources/**",
                 "/v3/api-docs/**",
-                "/test/**"
+                "/test/**",
+                "/member/loginpage"
 //                "/oauth2/authorization/kakao"
 //               ,"/**"
         );
