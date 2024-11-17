615d5232afea1f677b38c9a4375e1e3409feea0e
임권 <limkwon18@gmail.com>
2024-11-17 21:53:26 +0900

    update

--- detection/CSV.py
+++ detection/CSV.py
@@ -3,10 +3,10 @@ import csv
 from cloth_detection import ClothesFinder
 
 cf = ClothesFinder()
-f = open("test2.csv", "r")
+f = open("test3.csv", "r")
 reader = csv.reader(f)
 lines = []
-ff = open("test3.csv", 'w', newline='')
+ff = open("test4.csv", 'w', newline='')
 wr = csv.writer(ff)
 
 flag = True
@@ -17,16 +17,16 @@ for line in reader:
         flag = False
         continue
 
-    url = line[3]
-    type_id = url.find("bottom")
-    if type_id == -1:
-        type_id = 0
-    else:
-        type_id = 1
+    # url = line[3]
+    # type_id = url.find("bottom")
+    # if type_id == -1:
+    #     type_id = 0
+    # else:
+    #     type_id = 1
 
-    res = cf.run(url, type_id)
-    line[6] = res.color_id + 1
-    # line[6] = str(int(line[6]) + 1)
+    # res = cf.run(url, type_id)
+    # line[6] = res.color_id - 1
+    line[6] = str(int(line[6]) - 1)
 
     lines.append(line)
 
