8acf33c4ced7e1532b95ad2722a0d91803f23b15
김성현 <shtony123@naver.com>
2024-11-18 14:33:06 +0900

    refactor: feed limit

--- backend/src/main/java/com/clofit/api/fitting/service/FittingService.java
+++ backend/src/main/java/com/clofit/api/fitting/service/FittingService.java
@@ -3,6 +3,8 @@ package com.clofit.api.fitting.service;
 import com.clofit.api.fitting.entity.Fitting;
 import com.clofit.api.fitting.request.FittingRequest;
 import com.clofit.api.fitting.request.FittingSearchRequest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
@@ -10,8 +12,8 @@ import java.util.concurrent.CompletableFuture;
 public interface FittingService {
     byte[] fitting(FittingRequest fittingRequest) throws Exception;
     CompletableFuture<byte[]> fittingMQ(FittingRequest fittingRequest);
-    List<Fitting> getPublicFittingList();
-    List<Fitting> getPublicFittingListByColor(Long colorId);
+    Page<Fitting> getPublicFittingList(Pageable pageable);
+    Page<Fitting> getPublicFittingListByColor(Long colorId, Pageable pageable);
     List<String> recentFitting(Long memberId);
 
 }
