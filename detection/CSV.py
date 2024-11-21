0e9a752c89cab3fcdef2511ddf2c6e783178a601
jiu <118248199+jiu6525@users.noreply.github.com>
2024-11-21 15:30:50 +0900

    Revert "Feature/be/upload"

--- detection/CSV.py
+++ detection/CSV.py
@@ -1,12 +1,10 @@
 import os
 import csv
-from cloth_detection import ClothesFinder
 
-cf = ClothesFinder()
-f = open("test3.csv", "r")
+f = open("test.csv", "r")
 reader = csv.reader(f)
 lines = []
-ff = open("test4.csv", 'w', newline='')
+ff = open("test2.csv", 'w', newline='')
 wr = csv.writer(ff)
 
 flag = True
@@ -17,17 +15,7 @@ for line in reader:
         flag = False
         continue
 
-    # url = line[3]
-    # type_id = url.find("bottom")
-    # if type_id == -1:
-    #     type_id = 0
-    # else:
-    #     type_id = 1
-
-    # res = cf.run(url, type_id)
-    # line[6] = res.color_id - 1
-    line[6] = str(int(line[6]) - 1)
-
+    line[6] = str(int(line[6]) + 1)
     lines.append(line)
 
 wr.writerows(lines)
